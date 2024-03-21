6dd0e733b460070a316d8c62ed28b4badff2616a
PratapPresolv360 <git config --global user.email>
2024-03-22 00:23:17 +0530

    build added

--- server/server/settings.py
+++ server/server/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'/client1/build')],
+        'DIRS': [os.path.join(BASE_DIR,'/client/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,7 +133,7 @@ APPEND_SLASH = False
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'client1/build/static')
+    os.path.join(BASE_DIR,'client/build/static')
 ]
 
 # Default primary key field type

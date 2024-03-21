7627a1d81b567b5ccea94a32b3aee1995f51c519
PratapPresolv360 <git config --global user.email>
2024-03-22 03:56:22 +0530

    client section

--- server/server/urls.py
+++ server/server/urls.py
@@ -26,3 +26,5 @@ urlpatterns = [
     path('api/', include(route.urls)),
     path('',views.index,name='index')
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
